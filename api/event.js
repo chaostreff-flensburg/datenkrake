11e4679687ea59079ce8872244de5fd0ae966a25
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-02-28 19:25:42 +0100

    Added mockup event api endpoint

---
+++ api/event.js
@@ -0,0 +1,8 @@
+var router = require('express').Router()
+
+/* GET user by ID. */
+router.get('/event/signup', function (req, res, next) {
+  res.end();
+})
+
+module.exports = router
--- api/index.js
+++ api/index.js
@@ -2,5 +2,6 @@ var router = require('express').Router()
 
 // Add USERS Routes
 router.use(require('./users'))
+router.use(require('./event'))
 
 module.exports = router
