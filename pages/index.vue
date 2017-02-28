c70309bc96fce8cde280720fee4b555b9c805482
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-02-28 19:26:03 +0100

    Removed html form

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,23 @@
   <section class="container">
     <h2>Eventname</h2>
     <p>Event Description</p>
-    <form class="registration" action="/api/event/signup" method="post">
+    <div class="registration">
       <input type="text" name="name" placeholder="Name / @Nickname">
       <input type="mail" name="email" value="" placeholder="mail@example.com">
       <textarea name="description" rows="8" cols="80" placeholder="Description"></textarea>
-      <button type="submit" name="submit">Submit</button>
-    </form>
+      <button v-on:click="signup" name="submit">Submit</button>
+    </div>
   </section>
 </template>
 
 <script>
-
+export default {
+  methods: {
+    signup: function() {
+      alert('Submit')
+    }
+  }
+}
 </script>
 
 <style scoped>
