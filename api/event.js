8d0521b27cbb751902a66c83bf34dde0f86eec3a
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-03-04 18:06:55 +0100

    Rename event signup route

--- api/event.js
+++ api/event.js
@@ -8,24 +8,27 @@ var db = new Datastore({ filename: __dirname + '/../db/event', autoload: true })
 
 let transporter = nodemailer.createTransport(mail);
 
-router.post('/event/signup', function (req, res, next) {
-  res.json(req.body);
-})
 
-router.post('/event/submit', function (req, res, next) {
+router.post('/event/signup', function (req, res, next) {
   var user = req.body
   user.confirmed = false
   db.insert(user, function(err, newUser) {
-    signupMail(newUser._id, newUser.mail)
+    signupMail(newUser._id, newUser.mail, newUser.name)
   })
 })
 
-function signupMail(userId, userMail) {
+router.get('/event/confirm/:id', function(req, res, next) {
+  db.update({_id: req.params.id}, {$set: {confirmed: true}}, {}, function() {
+  })
+})
+
+
+function signupMail(userId, userMail, userName) {
   var message = {
     from: mail.auth.user,
     to: userMail,
     subject: 'Confirm your signup for ' + config.event.name,
-    text: 'Click here to confirm your signup: ' + config.host + '/api/event/confirm/' + userId
+    text: 'Hey ' + userName + '! Click here to confirm your signup: http://' + config.host + '/api/event/confirm/' + userId
   };
   transporter.sendMail(message)
 }
