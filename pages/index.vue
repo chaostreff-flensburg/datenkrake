4c97dee659a22edaae40530f1872829c6c4250cd
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-03-04 20:54:36 +0100

    Send form data to server via axios

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
     <h2>Eventname</h2>
     <p>Event Description</p>
     <div class="registration">
-      <input type="text" name="name" placeholder="Name / @Nickname">
-      <input type="mail" name="email" value="" placeholder="mail@example.com">
-      <textarea name="description" rows="8" cols="80" placeholder="Description"></textarea>
+      <input type="text" name="name" v-model="user.name" placeholder="Name / @Nickname">
+      <input type="mail" name="email" v-model="user.mail" placeholder="mail@example.com">
+      <textarea name="description" rows="8" cols="80" v-model="user.desc" placeholder="Description"></textarea>
       <button v-on:click="signup" name="submit">Submit</button>
     </div>
   </section>
@@ -15,9 +15,28 @@
 import axios from '~plugins/axios'
 
 export default {
+  data: function() {
+    return {
+      user: {
+        name:'',
+        mail:'',
+        desc:''
+      }
+    }
+  },
   methods: {
     signup: function() {
-      alert('Submit')
+      axios.post('/api/event/signup', {
+        name: this.user.name,
+        mail: this.user.mail,
+        desc: this.user.desc
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
     }
   }
 }
