e7262096724682c2bdde2ec01a4effac774ae81c
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-03-05 14:35:43 +0100

    Changed default head values

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,10 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'Datenkrake Eventanmeldung',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
     link: [
       { href:"https://fonts.googleapis.com/css?family=Arvo:400,700", rel:"stylesheet"},
