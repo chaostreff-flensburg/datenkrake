af5b506e752f9649f9bc45eef57088692cd9d32f
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-03-28 12:49:15 +0200

    Added comment referencing poor choices made in #3

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'weekendHACK - Chaostreff Flensburg',
+    title: 'weekendHACK - Chaostreff Flensburg', // TODO: get title from config
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
