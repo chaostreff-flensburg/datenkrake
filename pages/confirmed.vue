72a5de7376b1fb8d038f45483a269659bb059444
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-03-28 13:14:22 +0200

    Hotfix paragraphs into articles, as #3 changed styles in weekend hack layout and changed p tags to articles sometimes, but not everywhere. Tags should be converted back to p, as they are not an article or section.

--- pages/confirmed.vue
+++ pages/confirmed.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <h1 style="text-align: center; margin-bottom: -1em; margin-top: 0;">Du bist angemeldet!</h1>
     <div class="description">
-      <p><strong>Herzlichen Glückwunsch</strong>, du bist angemeldet!</p>
+      <article><strong>Herzlichen Glückwunsch</strong>, du bist angemeldet!</article>
     </div>
   </section>
 </template>
--- pages/submited.vue
+++ pages/submited.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <h1 style="text-align: center; margin-bottom: -1em; margin-top: 0;">Fast geschafft!</h1>
     <div class="description">
-      <p>Wir haben Dir eine <strong>Email</strong> mit einem Bestätigungslink gesendet. Bitte klicke auf diesen Link, um Deine Teilnahme zu bestätigen. Solltest Du keine Email erhalten haben, warte bitte ein paar Minuten oder schaue in Deinem <strong>Spam-Ordner</strong> nach.</p>
+      <article>Wir haben Dir eine <strong>Email</strong> mit einem Bestätigungslink gesendet. Bitte klicke auf diesen Link, um Deine Teilnahme zu bestätigen. Solltest Du keine Email erhalten haben, warte bitte ein paar Minuten oder schaue in Deinem <strong>Spam-Ordner</strong> nach.</article>
     </div>
   </section>
 </template>
