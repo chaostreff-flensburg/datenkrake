297938e587855b4c761fa1f60bbb305950e22bbf
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-05-09 21:40:22 +0200

    Content

--- pages/submitted.vue
+++ pages/submitted.vue
@@ -1,8 +1,7 @@
 <template>
   <section class="container">
-    <h1 style="text-align: center; margin-bottom: -1em; margin-top: 0;">Fast geschafft!</h1>
     <div class="description">
-      <article>Wir haben Dir eine <strong>Email</strong> mit einem Bestätigungslink gesendet. Bitte klicke auf diesen Link, um Deine Teilnahme zu bestätigen. Solltest Du keine Email erhalten haben, warte bitte ein paar Minuten oder schaue in Deinem <strong>Spam-Ordner</strong> nach.</article>
+      <article><h1>Fast geschafft!</h1>Wir haben Dir eine <strong>Bestätigungsmail</strong> gesendet. Solltest Du keine Email erhalten haben, warte bitte ein paar Minuten oder schaue in Deinem <strong>Spam-Ordner</strong> nach.</article>
     </div>
   </section>
 </template>
